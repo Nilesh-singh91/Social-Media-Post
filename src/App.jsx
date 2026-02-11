da13acba47ef6cb4e438fe2eceb0edb36d6960b7
Nilesh Singh <nileshkumarsinghn3@gmail.com>
2026-02-11 17:24:23 +0530

    fixed linux case issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
-import CreatePost from "./components/CreatePost.jsx";
+import CreatePost from "./components/createPost.jsx";
 import PostList from "./components/PostList";
 import { useState } from "react";
 import PostListProvider from "./store/post-list-store";
