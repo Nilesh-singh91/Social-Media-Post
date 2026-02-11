c390db7c35e82ad0a06f85f1f9a8162fff02d99d
Nilesh Singh <nileshkumarsinghn3@gmail.com>
2026-02-11 17:10:37 +0530

    fixed CreatePost import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
-import CreatePost from "./components/CreatePost";
+import CreatePost from "./components/CreatePost.jsx";
 import PostList from "./components/PostList";
 import { useState } from "react";
 import PostListProvider from "./store/post-list-store";
